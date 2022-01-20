3ffe10cb726816c375c57d86d5be7648b014fbb3
Ben Eger <bteger28@gmail.com>
2022-01-20 13:43:00 -0500

    initial commit

---
+++ sample1.js
@@ -0,0 +1,17 @@
+function validate() {
+
+    alert('hello')
+}
+
+function passwordsMatch() {
+    return false
+}
+
+function noEmptyInputFields() {
+    return false
+}
+
+function validateUsername() {
+    return false
+}
+
