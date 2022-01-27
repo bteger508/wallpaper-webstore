a3ab645a53ce550c48dde6cf7473ed11b7203d00
Ben Eger <bteger28@gmail.com>
2022-01-27 13:37:23 -0500

    Add php file to catch the registration post req

---
+++ sample1.php
@@ -0,0 +1,11 @@
+<?php
+    $first_name = $_POST['fname'];
+    $last_name = $_POST['lname'];
+    $password = $_POST['password'];
+    $pw_confirm = $_POST['pw-confirm'];
+    $phone = $_POST['phone'];
+    $email = $_POST['email'];
+
+    echo $email;
+
+?>
