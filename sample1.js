85cb11be25da65f78c526f10d00f84ef81e7e23e
Ben Eger <bteger28@gmail.com>
2022-01-27 13:12:27 -0500

    Add password match and length validation

--- sample1.js
+++ sample1.js
@@ -1,22 +1,44 @@
+function validateAll() {
+    errors = [];
+    if (!validateNoEmptyFields()) {
+        errors.push("No empty fields")
+    }
+    if (!passwordsMatch()) {
+        errors.push("Passwords must match")
+    }
+    if (!passwordLength) {
+        errors.push("Password must be at least 12 characters long")
+    }
 
+    if (errors.length > 0 ) {
+        console.error(errors)
+        return false
+    }
+
+    return true
+}
 
 function validateNoEmptyFields() {
-    alert("hello")
-    // Doesn't quite work yet
     myForm = document.getElementsByTagName('form')[0]
     for (elt in myForm.elements) {
         if (myForm.elements[elt].value == '') {
             console.log(myForm.elements[elt])
             alert('Cannot leave a field blank')
-            return
+            return false
         }
     }
+
+    return true
 }
 
 function passwordsMatch() {
     password = document.getElementById('password').value
     passwordConfirm = document.getElementById('pw-confirm').value
     return password === passwordConfirm
+}
+
+function passwordLength() {
+    return document.getElementById('password').value.length >= 12
 }
 
 function noEmptyInputFields() {
