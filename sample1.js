105d18680eee7c44a848b54e0ebb7266081afc2e
Ben Eger <bteger28@gmail.com>
2022-01-27 12:59:48 -0500

    add no empty field validation

--- sample1.js
+++ sample1.js
@@ -1,10 +1,22 @@
-function validate() {
 
-    alert('hello')
+
+function validateNoEmptyFields() {
+    alert("hello")
+    // Doesn't quite work yet
+    myForm = document.getElementsByTagName('form')[0]
+    for (elt in myForm.elements) {
+        if (myForm.elements[elt].value == '') {
+            console.log(myForm.elements[elt])
+            alert('Cannot leave a field blank')
+            return
+        }
+    }
 }
 
 function passwordsMatch() {
-    return false
+    password = document.getElementById('password').value
+    passwordConfirm = document.getElementById('pw-confirm').value
+    return password === passwordConfirm
 }
 
 function noEmptyInputFields() {
