56e3e2f254c5787d48d53f93ca8fe28498598a45
Ben Eger <bteger28@gmail.com>
2022-02-08 20:32:23 -0500

    Clientside validation
    Co-authored-by: Hunter Durbin <hunterdurbin@users.noreply.github.com>
    Co-authored-by: 423rd <423rd@users.noreply.github.com>

---
+++ login_validation.php
@@ -0,0 +1,19 @@
+<?php
+
+
+
+echo "welcome";
+
+$first_name = $_POST['fname'];
+$last_name = $_POST['lname'];
+$password = $_POST['password'];
+$pw_confirm = $_POST['pw-confirm'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+
+echo $first_name;
+echo $last_name;
+echo $password;
+echo $pw_confirm;
+echo $phone;
+echo $email;
