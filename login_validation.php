ce86bf9f826a554b620959ce6ced292c95c76122
Ben Eger <bteger28@gmail.com>
2022-02-08 21:00:30 -0500

    Add serverside validation
    Co-authored-by: Hunter Durbin <hunterdurbin@users.noreply.github.com>
    Co-authored-by: 423rd <423rd@users.noreply.github.com>

--- login_validation.php
+++ login_validation.php
@@ -1,19 +1,34 @@
 <?php
 
+$errors = [];
+$username = $first_name = $last_name = $password = $pw_confirm = $phone = $email;
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = test_input($_POST['username']);
+    $first_name = test_input($_POST['fname']);
+    $last_name = test_input($_POST['lname']);
+    $password = test_input($_POST['password']);
+    $pw_confirm = test_input($_POST['pwConfirm']);
+    $phone = test_input($_POST['phone']);
+    $email = test_input($_POST['email']);
+    $errors = [];
+
+    if ($username == "") {
+        array_push($errors, "Username cannot be empty");
+    }
+    echo $first_name;
+    echo $last_name;
+    echo $password;
+    echo $pw_confirm;
+    echo $phone;
+    echo $email;
+}
 
 
-echo "welcome";
-
-$first_name = $_POST['fname'];
-$last_name = $_POST['lname'];
-$password = $_POST['password'];
-$pw_confirm = $_POST['pw-confirm'];
-$phone = $_POST['phone'];
-$email = $_POST['email'];
-
-echo $first_name;
-echo $last_name;
-echo $password;
-echo $pw_confirm;
-echo $phone;
-echo $email;
+function test_input($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
