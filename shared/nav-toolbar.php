327e84e18181a475d43803900fc9f13e481b42cc
Hunter Durbin <hmdurbin@bsu.edu>
2022-03-01 18:40:47 -0500

    Add shared toolbar for nav

---
+++ shared/nav-toolbar.php
@@ -0,0 +1,13 @@
+<nav class="container-fluid">
+    <div class="row justify-content-between">
+        <div class="col">
+            <span class="h2 p-2">Wallpaper Webstore</span>
+        </div>
+        <div class="col col-auto">
+            <div class="row">
+                <a id="login_route" class="nav-link" href="../login-page">Login</a>
+                <a id="register_route" class="nav-link" href="../register-page">Register</a>
+            </div>
+        </div>
+    </div>
+</nav>
