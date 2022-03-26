f0f6d1a9a90860c4deb15fb6f0a48e2a15510da7
423rd <kmmatchett@bsu.edu>
2022-03-26 16:49:46 -0400

    Fix top static header styling

---
+++ browse-page/index.js
@@ -0,0 +1,8 @@
+// ====== ADD ACTIVE ROUTE LINK ==========
+function addActiveRoute() {
+	let element = document.getElementById('browse_route')
+	element.classList.add('active')
+}
+$(document).ready(function () {
+	addActiveRoute()
+})
