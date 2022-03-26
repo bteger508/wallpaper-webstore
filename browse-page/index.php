ec9486273e36dcfa47233e78e9e9c7a8259c5cb1
423rd <kmmatchett@bsu.edu>
2022-03-26 19:06:45 -0400

    Static image display. Needs to become dynamic in future commits.

--- browse-page/index.php
+++ browse-page/index.php
@@ -32,6 +32,10 @@ if (!defined('ROOT_DIR')) {
 
         <h1>Browse Page</h1>
 
+        <div class="testPictureContainer">
+            <img src="http://csmadison.dhcp.bsu.edu/~kmmatchett/wallpaper-webstore/resources/backyard.jpeg" alt="backyard" width="806.4" height="604.8"> <!-- 4032 3024Image downscaled factor of 10 -->
+
+
 		<!-- Optional JavaScript -->
 		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
 		<script
