6446110395aea81f11be373db978860abcdcea50
Ben Eger <bteger28@gmail.com>
2022-03-29 15:51:02 -0400

    Set userdata cookie on login

--- utils/php/cookies.php
+++ utils/php/cookies.php
@@ -1,8 +1,10 @@
 <?php
-
 if (!defined('ROOT_DIR')) {
     DEFINE('ROOT_DIR', __DIR__ . '/../../');
 }
+
+// These wrapper functions only work in certain instances, which I do not understand yet...
+// If they do not work, just copy their function body.
 
 function getFavoriteTag()
 {
@@ -11,10 +13,10 @@ function getFavoriteTag()
 
 function setFavoriteTag($tag)
 {
-    setcookie('fav-tag', $tag);
+    setcookie('fav-tag', $tag, 0, "/");
 }
 
-function setUserId($userId)
+function setUserData($userDataJson)
 {
-    setcookie('user_id', $userId);
+    setcookie('userData', $userDataJson, 0, "/");
 }
