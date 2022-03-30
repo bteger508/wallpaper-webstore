eec98c38d819a30e2fa3c70ef6acedddd35389a2
Hunter Durbin <hmdurbin@bsu.edu>
2022-03-29 23:59:29 -0400

    Add active route styling

---
+++ insert-product-page/index.js
@@ -0,0 +1,8 @@
+// ====== ADD ACTIVE ROUTE LINK ==========
+function addActiveRoute() {
+	let element = document.getElementById('insert_route');
+	element.classList.add('active')
+}
+$(document).ready(function () {
+	addActiveRoute()
+})
